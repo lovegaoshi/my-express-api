1f6d1b6e3a02331987c61f375750c1e740270166
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2025-03-13 15:31:14 -0700

    feat: init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import fs from "fs";
 import getPlayer from "./ytbi/store";
 
-getPlayer("getNew").then((data) =>
+getPlayer("getNew").then((data) => {
   // @ts-expect-error
-  fs.writeFile(`./cachedPlayers/${data.id}`, data.data, () => undefined)
-);
+  fs.writeFile(`./cachedPlayers/${data.id}`, data.data, () => undefined);
+  fs.writeFile(`./cachedPlayers/latest`, data.id, () => undefined);
+});
